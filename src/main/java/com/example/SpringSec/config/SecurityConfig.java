9b4e260238dff84120f96e7f17a956d5718aabb4
omarsalem22 <omarmoahamed25112001@gmail.com>
2025-07-04 21:13:49 +0300

    Spring Security Configuration

--- src/main/java/com/example/SpringSec/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSec/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.example.SpringSec.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -20,6 +30,26 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
 
+    }
+
+    // @Bean
+    // UserDetailsService userDetailsService() {
+
+    //     UserDetails user1 = User.withDefaultPasswordEncoder()
+    //             .username("aya")
+    //             .password("0000")
+    //             .roles("ADMIN")
+    //             .build();
+
+    //     return new InMemoryUserDetailsManager(user1);
+    // }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
 
 }
